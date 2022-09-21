c5b546ba169dfb05372bf90cc36716d514691150
“Housam <housam.kak20@gmail.com>
2022-09-21 20:20:15 +0300

    Completed responsiveness for the modals

--- landing-page.js
+++ landing-page.js
@@ -19,3 +19,37 @@ signup_close.onclick = function () {
 };
 
 ////////////////////////////////////////////////////////////////////
+
+// Get the Sign-up Modal
+///////////////////////////////////////////////////////////////////
+var signin_modal = document.querySelector("#myModal-signin");
+
+// Get the button that opens the modal
+var signin_btn = document.querySelector("#outer-login");
+
+// Get the <span> element that closes the modal
+var signin_close = document.querySelector("#signin-close");
+
+// When the user clicks the button, open the modal
+signin_btn.onclick = function () {
+  signin_modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+signin_close.onclick = function () {
+  signin_modal.style.display = "none";
+};
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// When the user clicks anywhere outside of any modal, close it
+window.onclick = function (event) {
+  if (event.target == signinmodal) {
+    signin_modal.style.display = "none";
+  }
+  if (event.target == signup_modal) {
+    signup_modal.style.display = "none";
+  }
+};
+////////////////////////////////////////////////////////////////////
