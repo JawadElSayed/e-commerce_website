9f5a105fe763a990961cab7019f8735d2da3114b
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-21 23:28:16 +0300

    favorite api

---
+++ favorite.php
@@ -0,0 +1,31 @@
+<?php
+
+include("connection.php");
+
+// inputs
+$client_id = $_POST["client_id"];
+$product_id = $_POST["product_id"];
+$response = [];
+
+// add favorite or unfavorite
+$favorite_sql = "INSERT INTO favorite(client_id, product_id) VALUE (?, ?)";
+$add = $mysqli->prepare($favorite_sql);
+$add->bind_param("ss",$client_id, $product_id);
+if ($add->execute() === TRUE) {
+    $response["status"] = "favorite";
+} 
+else {
+    $unfavorite_sql = "DELETE FROM favorite WHERE client_id = ? and product_id = ?";
+    $remove = $mysqli->prepare($unfavorite_sql);
+    $remove->bind_param("ss",$client_id, $product_id);
+    if ($remove->execute() === TRUE) {
+        $response["status"] = "unfavorite";
+    } 
+    else {
+        $response["success"] = "Error : " . mysqli_error($mysqli);
+        echo json_encode($response);
+        exit();
+    }
+}
+
+?>
