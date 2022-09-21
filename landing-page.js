ea54dac2aa2141d8286ee9a1b5e0f1bce140e1bd
“Housam <housam.kak20@gmail.com>
2022-09-21 17:24:57 +0300

    Added sign up modal

---
+++ landing-page.js
@@ -0,0 +1,21 @@
+// Get the Sign-up Modal
+///////////////////////////////////////////////////////////////////
+var signup_modal = document.querySelector("#myModal-signup");
+
+// Get the button that opens the modal
+var signup_btn = document.querySelector("#sign-up");
+
+// Get the <span> element that closes the modal
+var signup_close = document.querySelector("#signup-close");
+
+// When the user clicks the button, open the modal
+signup_btn.onclick = function () {
+  signup_modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+signup_close.onclick = function () {
+  signup_modal.style.display = "none";
+};
+
+////////////////////////////////////////////////////////////////////
