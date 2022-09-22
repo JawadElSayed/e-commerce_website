27ce71101f59372a8a065d77b7cb38133cac6ebb
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-22 22:19:14 +0300

    set update to profile

--- edit_profile.php
+++ edit_profile.php
@@ -28,4 +28,32 @@ function encode_image ($path){
     return $base64;
 }
 
+// checking change of profile image
+$old_profile_sql = "SELECT profile FROM users WHERE id = ?";
+$old_profile_url = $mysqli->prepare($old_profile_sql);
+$old_profile_url->bind_param("s", $client_id);
+$old_profile_url->execute();
+$old_profile = $old_profile_url->get_result()->fetch_object()->profile;
+$old_profile_base64 = encode_image($old_profile);
+
+if ($profile != $old_profile_base64){
+    $new_profile_img = decode_image($profile,"images/profile/");
+}
+else{
+    $new_profile_img = $old_profile;
+}
+
+// set update
+$update_sql = "UPDATE users
+                SET name = ?,
+                profile = ?
+                WHERE id = ?";
+$add = $mysqli->prepare($update_sql);
+$add->bind_param("sss", $name, $new_profile_img, $client_id);
+$add->execute();
+
+$response["status"] = "Done";
+
+echo json_encode($response);
+
 ?>
