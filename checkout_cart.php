8875e7bfb1c108ed262be7f0cc43f59eeed79468
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-22 16:46:32 +0300

    add cart to checkout

--- checkout_cart.php
+++ checkout_cart.php
@@ -4,10 +4,11 @@ include("connection.php");
 
 // variables
 $client_id = $_POST["client_id"];
+$result = [];
 $response = [];
 
 // select cart
-$select_sql = "SELECT client_id, product_id, quantity 
+$select_sql = "SELECT product_id, quantity 
             FROM `cart` 
             WHERE client_id = ?";
 $select = $mysqli->prepare($select_sql);
@@ -16,9 +17,19 @@ $select->execute();
 $array = $select->get_result();
 
 while($a = $array->fetch_assoc()){
-    $response[] = $a;
+    $result[] = $a;
 }
 
+// add to checkout table
+$add_sql = "INSERT INTO checkouts(client_id, product_id, quantity) VALUE (?, ?, ?)";
+$add = $mysqli->prepare($add_sql);
+
+foreach ($result as $value){
+    $add->bind_param("sss", $client_id, $value["product_id"], $value["quantity"]);
+    $add->execute();
+}
+
+$response["status"] = "Done";
 echo json_encode($response);
 
 ?>
