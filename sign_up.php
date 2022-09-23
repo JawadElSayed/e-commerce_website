16b1d95fdb130c2a1d90b192fd099525a57e7743
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-23 09:51:46 +0300

    put the check of username first in login api

--- sign_up.php
+++ sign_up.php
@@ -10,18 +10,6 @@ $profile = "images/profile/default.png";
 $user_type = $_POST["user_type"];
 $response = [];
 
-// checking email if used
-$check_email_sql = "SELECT email FROM users WHERE email =? ";
-$select = $mysqli->prepare($check_email_sql);
-$select->bind_param("s", $email);
-$select->execute();
-$email_check = $select->get_result();
-
-if(mysqli_num_rows($email_check)) {
-    $response["status"] = "used email";
-    exit($json = json_encode($response));
-}
-
 // checking username if used
 $check_username_sql = "SELECT username FROM users WHERE username =? ";
 $select = $mysqli->prepare($check_username_sql);
@@ -31,6 +19,18 @@ $username_check = $select->get_result();
 
 if(mysqli_num_rows($username_check)) {
     $response["status"] = "used username";
+    exit($json = json_encode($response));
+}
+
+// checking email if used
+$check_email_sql = "SELECT email FROM users WHERE email =? ";
+$select = $mysqli->prepare($check_email_sql);
+$select->bind_param("s", $email);
+$select->execute();
+$email_check = $select->get_result();
+
+if(mysqli_num_rows($email_check)) {
+    $response["status"] = "used email";
     exit($json = json_encode($response));
 }
 
