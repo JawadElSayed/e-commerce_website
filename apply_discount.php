59744cb41c3bb2f31ba7d83252f9ba54333da6f2
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-23 09:49:29 +0300

    apply discount api finish

--- apply_discount.php
+++ apply_discount.php
@@ -5,9 +5,7 @@ include("connection.php");
 // variables
 $product_id = $_POST["product_id"];
 $discount_code = $_POST["discount_code"];
-
-echo $product_id;
-echo $discount_code;
+$response = [];
 
 // checking discount
 $sql = "SELECT products.price, discount_codes.discount_amount
@@ -23,5 +21,16 @@ if(!(mysqli_num_rows($code_check))) {
     $response["status"] = "invalid code";
     exit(json_encode($response));
 }
+
+// apply discound
+
+while($a = $code_check->fetch_assoc()){
+    $new_price = $a["price"] - $a["price"] * $a["discount_amount"] / 100 ;
+}
+
+$response["status"] = "correct";
+$response["new_price"] = $new_price;
+
+echo json_encode($response);
 
 ?>
