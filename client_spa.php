a57382a3df5f991f3163322f8ae8e14b21aff9df
jawad alsayed <jawadelsayed3@gmail.com>
2022-09-24 03:28:31 +0300

    getting wishlist products details

--- client_spa.php
+++ client_spa.php
@@ -124,7 +124,48 @@ function favorite_products($mysqli, $id){
     return $final_response;
 }
 
-echo json_encode(favorite_products($mysqli, $client_id), JSON_UNESCAPED_SLASHES);
+// getting wishlist products
+function wishlist_products($mysqli, $id){
+    $final_response = [];
+    $products_response = [];
+    // product info
+    $products_sql = "SELECT products.id, product_name, about, price, users.name, users.email
+                    FROM `products`
+                    INNER JOIN users ON products.seller_id = users.id
+                    INNER JOIN wish_list ON products.id = wish_list.product_id
+                    WHERE wish_list.client_id = ?";
+    $select_product = $mysqli->prepare($products_sql);
+    $select_product->bind_param("s", $id);
+    $select_product->execute();
+    $profile_array = $select_product->get_result();
+    while($a = $profile_array->fetch_assoc()){
+
+        $products_response["id"]=$a["id"];
+        $products_response["product_name"]=$a["product_name"];
+        $products_response["about"]=$a["about"];
+        $products_response["price"]=$a["price"];
+        $products_response["name"]=$a["name"];
+        $products_response["email"]=$a["email"];
+
+        // product images
+        $images_sql = "SELECT id , image
+                        FROM `images`
+                        WHERE product_id = ?";
+        $select_images = $mysqli->prepare($images_sql);
+        $select_images->bind_param("s", $a["id"]);
+        $select_images->execute();
+        $images_array = $select_images->get_result();
+        $images_response = [];
+        while ($b = $images_array->fetch_assoc()){
+            $images_response[] = $b;
+        }
+        $products_response["images"] = $images_response;
+        $final_response[] = $products_response;
+    }
+    return $final_response;
+}
+
+echo json_encode(wishlist_products($mysqli, $client_id), JSON_UNESCAPED_SLASHES);
 
 
 ?>
