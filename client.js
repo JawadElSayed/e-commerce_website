892ab1092660032ad4c90cf6223fbe4ab0d81f4e
“Housam <housam.kak20@gmail.com>
2022-09-24 19:24:37 +0300

    Bringing the ajax calling command for reference

--- client.js
+++ client.js
@@ -10,6 +10,29 @@ wishlist.addEventListener("click", Wishlist);
 inbox.addEventListener("click", Inbox);
 cart.addEventListener("click", Cart);
 
+// window.onload = () => {
+//   const cards = document.querySelector("#news");
+//   allcomponent = "";
+//   $.ajax({
+//     url: "http://localhost/Backend/news/news.php",
+//     success: function (data) {
+//       data = JSON.parse(data);
+//       for (let i = 0; i < data.length; i++) {
+//         title = data[i].title;
+//         text = data[i].text;
+//         component = `<div class="card" style="width: 18rem;">
+//               <div class="card-body">
+//                 <h5 class="card-title">${title}</h5>
+//                 <p class="card-text">${text}</p>
+//               </div>
+//             </div>`;
+//         allcomponent += component;
+//       }
+//       cards.innerHTML += allcomponent;
+//     },
+//   });
+// };
+
 function Home() {
   // Animation of the ads
   let slideIndex = 0;
