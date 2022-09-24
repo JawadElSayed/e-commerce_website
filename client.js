9091612a01beab89ddb6e9b322b78743dd861034
“Housam <housam.kak20@gmail.com>
2022-09-24 19:02:59 +0300

    Structured the javascript for spa

--- client.js
+++ client.js
@@ -1,22 +1,41 @@
-// Animation of the ads
-let slideIndex = 0;
-showSlides();
+const home = document.querySelector("#Home-txt");
+const favoriteS = document.querySelector("#Favorites-txt");
+const wishlist = document.querySelector("#Wishlist-txt");
+const inbox = document.querySelector("#Inbox-txt");
+const cart = document.querySelector("#cart-im");
+Home();
+home.addEventListener("click", Home);
+favoriteS.addEventListener("click", Favorites);
+wishlist.addEventListener("click", Wishlist);
+inbox.addEventListener("click", Inbox);
+cart.addEventListener("click", Cart);
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function Home() {
+  // Animation of the ads
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+function Favorites() {}
+function Wishlist() {}
+function Inbox() {}
+function Cart() {}
